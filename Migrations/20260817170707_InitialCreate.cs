0a55fc45a5db3996567cb68cb10c14fb97cc0262
Omar Osvaldo Orozco Rodriguez <omar.orozco@ferromex.mx>
2026-08-17 11:20:50 -0600

    Se creó la base de datos para el ambiente de desarrollo y se agregó la configuración necesaria para apuntar a la misma.

---
+++ Migrations/20260817170707_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PacificStarBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "unidades",
+                columns: table => new
+                {
+                    numero_unidad = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    horas_motor = table.Column<int>(type: "int", nullable: true),
+                    modelo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_unidades", x => x.numero_unidad);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bitacoras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    numero_unidad = table.Column<int>(type: "int", nullable: false),
+                    nivel_combustible = table.Column<decimal>(type: "decimal(5,2)", precision: 5, scale: 2, nullable: false),
+                    hora_encendido = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    temperatura_inicial = table.Column<decimal>(type: "decimal(5,2)", precision: 5, scale: 2, nullable: false),
+                    temperatura_final = table.Column<decimal>(type: "decimal(5,2)", precision: 5, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bitacoras", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_bitacoras_unidades_numero_unidad",
+                        column: x => x.numero_unidad,
+                        principalTable: "unidades",
+                        principalColumn: "numero_unidad",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bitacoras_numero_unidad",
+                table: "bitacoras",
+                column: "numero_unidad");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "bitacoras");
+
+            migrationBuilder.DropTable(
+                name: "unidades");
+        }
+    }
+}
